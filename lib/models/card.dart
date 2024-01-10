40d854b02052ef1336b01c978eea859026a04758
Youri Bakker <youri.bakker@trimm.nl>
2024-01-10 13:27:57 +0100

    fix: added a few fields

--- lib/models/card.dart
+++ lib/models/card.dart
@@ -7,11 +7,24 @@ part 'card.g.dart';
 @freezed
 class Card with _$Card {
   const factory Card({
+    // Core Card fields
     @UUID() required String id,
     // ignore: invalid_annotation_target
     @JsonKey(name: 'object') required String type,
-    required String title,
-    required DateTime releaseDate,
+    @UUID() String? oracleId,
+    List<int>? multiverseIds,
+    int? mtgoId,
+    int? tcgplayerId,
+    int? cardmarketId,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'lang') String language,
+
+    // Gameplay fields
+    required String name,
+
+    // Print fields
+    // TODO
+    required DateTime releasedAt,
   }) = _Card;
 
   factory Card.fromJson(Map<String, Object?> json) => _$CardFromJson(json);
