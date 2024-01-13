dce30cdc5829b5d966ccd16fab73b3098848f5ee
Youri  Bakker <youriboulanger@gmail.com>
2024-01-13 16:44:34 +0100

    feat: did some things

--- lib/pages/navigation_pages/search.dart
+++ lib/pages/navigation_pages/search.dart
@@ -55,17 +55,15 @@ class _SearchState extends State<Search> {
             ),
           ),
           query.length >= 3
-              ? Expanded(
-                  child: SimpleAsyncBuilder<CardList>(
-                    future: client.searchCards(Uri.encodeComponent(query)),
-                    onLoad: (CardList matchedCards, BuildContext context) =>
-                        Expanded(
-                      child: ListView.builder(
-                          itemCount: matchedCards.data.length,
-                          padding: EdgeInsets.zero,
-                          itemBuilder: (BuildContext context, int index) =>
-                              Text(matchedCards.data[index].name)),
-                    ),
+              ? SimpleAsyncBuilder<CardList>(
+                  future: client.searchCards(Uri.encodeComponent(query)),
+                  onLoad: (CardList matchedCards, BuildContext context) =>
+                      Expanded(
+                    child: ListView.builder(
+                        itemCount: matchedCards.data.length,
+                        padding: EdgeInsets.zero,
+                        itemBuilder: (BuildContext context, int index) =>
+                            Text(matchedCards.data[index].name)),
                   ),
                 )
               : Text('Zoeken'),
